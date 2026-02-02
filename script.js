7ba327f194e35b839727f4a7fcafdf65e0b712e1
janania236 <janania236@gmail.com>
2026-02-02 12:33:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function startParty() {
+  document.getElementById("start").classList.add("hidden");
+  document.getElementById("party").classList.remove("hidden");
+  startFireworks(); // we’ll make fireworks later
+}
+
+function notYou() {
+  alert("😂 Haha nice try! If you are NOT the birthday boy, this place is not for you 😏");
+}
+
+function startFireworks() {
+  // placeholder for fireworks
+  console.log("Fireworks start!");
+}
